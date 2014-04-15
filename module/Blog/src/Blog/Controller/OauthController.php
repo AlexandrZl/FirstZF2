26e07493dece556827a607528994a87f90c54b73
AlexandrZl <geedisgood72@gmail.com>
2014-04-15 12:03:06 +0700

    ZendOAuth Consumer

--- module/Blog/src/Blog/Controller/OauthController.php
+++ module/Blog/src/Blog/Controller/OauthController.php
@@ -9,17 +9,29 @@ class OauthController extends AbstractActionController
 {
     public function loginAction()
     {
+        ini_set('display_errors',1);
+        session_start();
         $config = array(
             'callbackUrl' => 'http://zf-blog',
-            'siteUrl' => 'http://api.twitter.com/oauth2',
-            'consumerKey' => "",
-            'consumerSecret' => "",
+            'consumerKey' => 'sSQ9DJU4rSBWx6ARQHpgA',
+            'consumerSecret' => 'UsnMpyatd0FmfkWvOof4I6yyMecxlgM9QxzocGDMWA',
+            'requestTokenUrl' => 'http://api.twitter.com/oauth2/request_token',
+            'authorizeUrl' => 'http://api.twitter.com/oauth2/authorize',
+            'accessTokenUrl' => 'http://api.twitter.com/oauth2/access_token',
         );
-
         $consumer = new Consumer($config);
-        $token = $consumer->getRequestToken(); 
-        $this->session->twitter_request_token = serialize($token); 
-        $consumer->redirect();
+        if (!isset($_SESSION['ACCESS_TOKEN'])) {
+            if (!empty($_GET)) {
+                $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN']));
+                $_SESSION['ACCESS_TOKEN'] = serialize($token);
+            } else {
+                $token = $consumer->getRequestToken();
+                $_SESSION['REQUEST_TOKEN'] = serialize($token);
+                $consumer->redirect();
+            }
+        } else {
+            $token = unserialize($_SESSION['ACCESS_TOKEN']);
+            $_SESSION['ACCESS_TOKEN'] = null;
+        }
     }
-    
 }
