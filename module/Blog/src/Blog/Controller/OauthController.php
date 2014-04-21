5ab7c8c55bcd6367432adfb05134da703afe7b05
AlexandrZl <geedisgood72@gmail.com>
2014-04-21 12:52:58 +0700

    ZendOAuth

--- module/Blog/src/Blog/Controller/OauthController.php
+++ module/Blog/src/Blog/Controller/OauthController.php
@@ -3,37 +3,29 @@ namespace Blog\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use ZendOAuth\Consumer;
-use ZendOAuth\Token\Access;
-use ZendService\Twitter\Twitter;
+
 
 class OauthController extends AbstractActionController
 {
+    public function config()
+    {
+       $configuration = array(
+           'callbackUrl' => 'http://zf-blog/oauth/callback',
+           'siteUrl' => 'http://api.twitter.com/oauth',
+           'consumerKey' => '03pANGHR0BsEh0lHd67O6htga',
+           'consumerSecret' => 'HWyEp0m7zAAf1T4IG2yPcO7RoDpWBEqf2myrbMnH9u8a8jOLb9'
+       );
+     
+       return $configuration;
+    }
     public function indexAction()
     {
         ini_set('display_errors',1);
-        $config = array(
-            'access_token' => array(
-                'token'  => '1715923526-SaO9r6YDjNVDeokyfrHfX89I3iv8NLPtqbGt7Pu',
-                'secret' => '3U1wF27Vuxkr2ieLKbiDz71L9Ui9Bzra1w1ShFB9uQ7pU',
-            ),
-            'oauth_options' => array(
-                'consumerKey' => 'EQ8wKIveUS2hZTKGAAMIH1LcS',
-                'consumerSecret' => 'EWKF1IQ1sGrJs3yHQRIBVkP6KM2DjkUaMY16pM6EolZ1WI1jsh',
-            ),
-            'http_client_options' => array(
-                'adapter' => 'Zend\Http\Client\Adapter\Curl',
-                'curloptions' => array(
-                    CURLOPT_SSL_VERIFYHOST => false,
-                    CURLOPT_SSL_VERIFYPEER => false,
-                ),
-            ),
-        );
-
-        $twitter = new Twitter($config);
-        $response = $twitter->account->verifyCredentials();
-        if ($response->isError()) {
-            die('Something is wrong with my credentials!');
-        }
+        session_start();
+        $consumer = new Consumer($this->config());
+        $token = $consumer->getRequestToken();
+        $_SESSION['TWITTER_REQUEST_TOKEN'] = serialize($token);
+        $consumer->redirect();
     }
 
     public function callbackAction()
