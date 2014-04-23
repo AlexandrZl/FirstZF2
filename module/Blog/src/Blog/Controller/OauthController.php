8b8a1f2348f5ac6f4653dc39e944fc82f5609764
AlexandrZl <geedisgood72@gmail.com>
2014-04-23 14:03:18 +0700

    Oauth2 twitter Mpdule:ZendService Oauth2

--- module/Blog/src/Blog/Controller/OauthController.php
+++ module/Blog/src/Blog/Controller/OauthController.php
@@ -3,19 +3,47 @@ namespace Blog\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use ZendOAuth\Consumer;
-use League\OAuth2\Client\Provider\Google;
+use ZendService\Oauth2\Client\Client;
 
 
 class OauthController extends AbstractActionController
 {
+    public function config()
+    {
+       $configuration = array(
+            'client' => array(
+                'client_id' => '2455247748',
+                'client_secret' => 'HWyEp0m7zAAf1T4IG2yPcO7RoDpWBEqf2myrbMnH9u8a8jOLb9',
+                'authorization_url' => 'https://api.twitter.com/oauth/authorize',
+                'access_token_url' => 'https://api.twitter.com/oauth/access_token',
+                'redirect_uri' => 'http://zf-blog/oauth/callback',
+                'state' => 'somerandomstate',
+            ),
+            'http' => array(
+                'adapter'   => 'Zend\Http\Client\Adapter\Curl',
+                'curloptions' => array(
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                ),
+            ),
+        );
+     
+       return $configuration;
+    }
+
     public function indexAction()
     {
-        $provider = new Google(array(
-            'clientId'  =>  '555205517183-0vj2f4g537giasn626kihllqkljtev74.apps.googleusercontent.com',
-            'clientSecret'  =>  'LKqC8sP00InoQBO3wOFukbmf',
-            'redirectUri'   =>  "http://127.0.0.1/oauth/index",
-        ));
+        $client = new Client($this->config());
+        $url = $client->getAuthorizationRequestUrl();
+        return $this->redirect()->toUrl($url);
+    }
 
-        $provider->authorize();
+    public function callbackAction()
+    {
+        $client = new Client($this->config());
+        $code = $_GET['code'];
+        $accessToken = $client->getAccessToken(array(
+            'code' => $code
+        ));
     }
 }
