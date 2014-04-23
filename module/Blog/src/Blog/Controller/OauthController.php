00a12f44092e6638d987b1700526b4400ddb35f8
AlexandrZl <geedisgood72@gmail.com>
2014-04-23 13:27:21 +0700

    Oauth2 Google

--- module/Blog/src/Blog/Controller/OauthController.php
+++ module/Blog/src/Blog/Controller/OauthController.php
@@ -3,39 +3,19 @@ namespace Blog\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use ZendOAuth\Consumer;
+use League\OAuth2\Client\Provider\Google;
 
 
 class OauthController extends AbstractActionController
 {
-    public function config()
-    {
-       $configuration = array(
-           'callbackUrl' => 'http://zf-blog/oauth/callback',
-           'siteUrl' => 'http://api.twitter.com/oauth',
-           'consumerKey' => '03pANGHR0BsEh0lHd67O6htga',
-           'consumerSecret' => 'HWyEp0m7zAAf1T4IG2yPcO7RoDpWBEqf2myrbMnH9u8a8jOLb9'
-       );
-     
-       return $configuration;
-    }
     public function indexAction()
     {
-        ini_set('display_errors',1);
-        session_start();
-        $consumer = new Consumer($this->config());
-        $token = $consumer->getRequestToken();
-        $_SESSION['TWITTER_REQUEST_TOKEN'] = serialize($token);
-        $consumer->redirect();
-    }
+        $provider = new Google(array(
+            'clientId'  =>  '555205517183-0vj2f4g537giasn626kihllqkljtev74.apps.googleusercontent.com',
+            'clientSecret'  =>  'LKqC8sP00InoQBO3wOFukbmf',
+            'redirectUri'   =>  "http://127.0.0.1/oauth/index",
+        ));
 
-    public function callbackAction()
-    {
-        session_start();
-        $consumer = new Consumer($this->config());
-        $token = $consumer->getAccessToken(
-            $_GET,
-            unserialize($_SESSION['TWITTER_REQUEST_TOKEN'])
-        );
-        $_SESSION['TWITTER_ACCESS_TOKEN'] = serialize($token);
+        $provider->authorize();
     }
 }
